03ca35412b2a2f29a32fbf1e7a5beade599d4858
myzhenhao <advaspire@gmail.com>
2024-05-01 16:15:05 +0800

    render update

--- main.py
+++ main.py
@@ -27,5 +27,5 @@ app.include_router(todos.router)
 #     import uvicorn
 #     uvicorn.run(app)
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
